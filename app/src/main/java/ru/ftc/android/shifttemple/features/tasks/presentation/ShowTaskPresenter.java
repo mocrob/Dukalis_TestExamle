c2b83e9559401e942b23b2303e220ce189adcbbd
Alexander <ktupin.alexander1@gmail.com>
2019-07-10 17:56:01 +0700

    5 files for ShowTask

---
+++ app/src/main/java/ru/ftc/android/shifttemple/features/tasks/presentation/ShowTaskPresenter.java
@@ -0,0 +1,8 @@
+package ru.ftc.android.shifttemple.features.tasks.presentation;
+
+import ru.ftc.android.shifttemple.features.MvpPresenter;
+
+
+public class ShowTaskPresenter extends MvpPresenter<ShowTaskView> {
+
+}
---
+++ app/src/main/java/ru/ftc/android/shifttemple/features/tasks/presentation/ShowTaskView.java
@@ -0,0 +1,11 @@
+package ru.ftc.android.shifttemple.features.tasks.presentation;
+
+import ru.ftc.android.shifttemple.features.MvpView;
+
+
+public interface ShowTaskView extends MvpView {
+
+    void showError();
+
+}
+
